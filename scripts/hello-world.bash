ef6eb6133219b84ccc7ac1a7e3badb0e6a4a0079
SebastianOpiyo <sebamopale@gmail.com>
2020-05-21 01:57:49 +0300

    Add scripting standards.

--- scripts/hello-world.bash
+++ scripts/hello-world.bash
@@ -1,1 +1,26 @@
+#!/bin/bash
+# Purpose: Hello world script
+# Date: May 21, 2020 01:40
+# Modification May 21, 2020 
+# Anything more ......
+
+# Define Variables, this facilitates reusability
+# e.g 
+name='Sebastian Opiyo'
+
+# We call the variable right here
+echo 'The Writer of this script is: ' $name
+
 echo 'Hello World...Running on borne again shell!'
+
+# We can do other commands right below e.g
+# date
+# df -h 
+
+# Statements can go right below:
+# For readability remember to tab, e.g.
+#	if {$a -eq file }
+#	then echo this
+#	else 
+#		echo that 
+#	fi
