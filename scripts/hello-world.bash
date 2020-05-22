d0b48ee638f80b50bd95d8b3eb5a1eca186d4ed4
SebastianOpiyo <sebamopale@gmail.com>
2020-05-22 21:14:26 +0300

    Clean up

--- scripts/hello-world.bash
+++ scripts/hello-world.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Purpose: Hello world script
 # Date: May 21, 2020 01:40
-# Modification May 21, 2020 
+# Modification May 22, 2020 
 # Anything more ......
 
 # Define Variables, this facilitates reusability
@@ -9,10 +9,12 @@
 name='Sebastian Opiyo'
 
 # We call the variable right here
+# We use acho with no argument to create space between lines.
+echo
 echo 'The Writer of this script is: ' $name
-
+echo
 echo 'Hello World...Running on borne again shell!'
-
+echo
 # We can do other commands right below e.g
 # date
 # df -h 
